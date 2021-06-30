b22c5fd10cd2896dcc8dedfc95f7dedc50ce234b
mhhutton <mhhutton@gmail.com>
2021-06-30 12:53:22 -0600

    server requires routes, connects to port

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Routes
+const qaRouter = require('../routes/qaRoutes');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/qa', qaRouter);
+
+const PORT = 5000 || process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
